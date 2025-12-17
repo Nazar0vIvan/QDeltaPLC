fd3806869cfe534274be8e497d31582376fbc650
BormachBoroda <vano888888@gmail.com>
2025-12-17 07:00:46 +0300

    chore: not essential changes

--- python/plot_forces.py
+++ python/plot_forces.py
@@ -2,7 +2,9 @@ import numpy as np
 import json
 import matplotlib.pyplot as plt
 
-with open("record.json", "r", encoding="utf-8") as f:
+
+# record.json 
+with open("record_scaled_fx_fy.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 samples = data["samples"]
@@ -12,22 +14,31 @@ t  = [s["timestamp"] for s in samples]
 scale = 1_000_000.0
 Fx = [s["Fx"] / scale for s in samples]
 Fy = [s["Fy"] / scale for s in samples]
-Fz = [s["Fz"] / scale for s in samples]
+#Fz = [s["Fz"] / scale for s in samples]
 Tx = [s["Tx"] / scale for s in samples]
 Ty = [s["Ty"] / scale for s in samples]
 Tz = [s["Tz"] / scale for s in samples]
 
 # Plot forces
+label_fs = 16
+tick_fs  = 14
+legend_fs = 14
+
 plt.figure()
 plt.plot(t, Fx, label="Fx")
 plt.plot(t, Fy, label="Fy")
-plt.plot(t, Fz, label="Fz")
-plt.xlabel("t, s")
-plt.ylabel("Force (scaled)")
-plt.title("Forces vs time")
-plt.legend()
-plt.grid(True)
 
+plt.xlabel("Время, с", fontsize=label_fs)
+plt.ylabel("Силы", fontsize=label_fs)
+
+plt.xticks(fontsize=tick_fs)
+plt.yticks(fontsize=tick_fs)
+
+plt.legend(fontsize=legend_fs)
+plt.grid(True)
+plt.show()
+
+'''
 # Plot torques
 plt.figure()
 plt.plot(t, Tx, label="Tx")
@@ -38,7 +49,7 @@ plt.ylabel("Torque (scaled)")
 plt.title("Torques vs time")
 plt.legend()
 plt.grid(True)
-
+'''
 plt.show()
 
 
